620ff41448c1d8431f4c1debea9b83fd198af978
Violeta <violetr.23@gmail.com>
2018-09-01 15:58:30 -0400

    Restructured Modules

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "../../../node_modules/@angular/core";
+import { Routes, RouterModule } from "../../../node_modules/@angular/router";
+import { SignupComponent } from "./signup/signup.component";
+import { SigninComponent } from "./signin/signin.component";
+
+const authRoutes: Routes = [
+    { path: 'signup', component: SignupComponent},
+    { path: 'signin', component: SigninComponent}
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(authRoutes)
+    ],
+    exports: [RouterModule]
+})
+
+export class AuthRoutingModule {}
